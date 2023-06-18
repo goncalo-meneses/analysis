0d328c82d8e94da919b67bc83b761b9d0f5fa03a
goncalo-meneses <schoolgoncalo1@gmail.com>
2023-06-18 22:54:02 +0100

    Another fitting session

--- main/gaussian2.cpp
+++ main/gaussian2.cpp
@@ -10,7 +10,7 @@
 void PlotGaussiansWithData()
 {
     // Read data from file
-    std::ifstream file("/home/goncalo/LFEA/analysis/main/cs.txt");
+    std::ifstream file("/home/goncalo/LFEA/analysis/main/co.txt");
     if (!file.is_open())
     {
         std::cout << "Failed to open data file." << std::endl;
@@ -36,7 +36,7 @@ void PlotGaussiansWithData()
     int numPoints = xValues.size();
     TGraphErrors *graph = new TGraphErrors(numPoints, &xValues[0], &yValues[0], 0, &yErrors[0]);
 
-    graph->SetTitle("Ajuste ao Espetro do ^{137}Cs");
+    graph->SetTitle("Ajuste ao Espetro do ^{60}Co");
     graph->GetXaxis()->SetTitle("Canal");
     graph->GetYaxis()->SetTitle("Contagens");
 
@@ -44,7 +44,7 @@ void PlotGaussiansWithData()
     graph->GetYaxis()->CenterTitle(true);
 
     // Create a canvas to display the plot
-    TCanvas *canvas = new TCanvas("canvas", "Gaussians with Data", 800, 600);
+    TCanvas *canvas = new TCanvas("canvas", "Gaussians with Data", 950, 600);
 
     // Draw the data points
     graph->SetMarkerStyle(20);
@@ -52,34 +52,49 @@ void PlotGaussiansWithData()
     graph->Draw("AP");
 
     // Create and draw the three Gaussian functions
-    TF1 *gaussian1 = new TF1("gaussian1", "[0]*TMath::Gaus(x, [1], [2])", 0, 170);
-    TF1 *gaussian2 = new TF1("gaussian2", "[0]*TMath::Gaus(x, [1], [2])", 0, 170);
-
+    TF1 *gaussian1 = new TF1("gaussian1", "[0]*TMath::Gaus(x, [1], [2])", 700, 1000);
+    TF1 *gaussian2 = new TF1("gaussian2", "[0]*TMath::Gaus(x, [1], [2])", 600, 900);
+    TF1 *gaussian3 = new TF1("gaussian3", "[0]*TMath::Gaus(x, [1], [2])", 0, 300);
+    TF1 *gaussian4 = new TF1("gaussian4", "[0]*TMath::Gaus(x, [1], [2])", 0, 300);
+    
     // Césio 237
-    gaussian1->SetParameters(1.63229e+03, 1.15777e+02, 3.44590e+00);
-    gaussian2->SetParameters(2.38294e+02, 1.25212e+02, 1.88109e+00);
+    gaussian1->SetParameters(3.73699e+02, 8.27303e+02, 1.62066e+01);
+    gaussian2->SetParameters(4.81812e+02, 7.28563e+02, 1.59784e+01);
+    gaussian3->SetParameters(6.15022e+02, 1.42145e+02, 2.65231e+01);
+    gaussian4->SetParameters(6.29325e+02, 1.18064e+02, 1.81869e+01);
 
     gaussian1->SetLineColor(kRed);
     gaussian2->SetLineColor(kAzure-6);
+    gaussian3->SetLineColor(kSpring-8);
+    gaussian4->SetLineColor(kOrange-2);
 
     gaussian1->Draw("same");
     gaussian2->Draw("same");
+    gaussian3->Draw("same");
+    gaussian4->Draw("same");
 
+    /* Sum gaussians?
     // Create and draw the sum of the Gaussians
-    TF1 *gaussianSum = new TF1("gaussianSum", "[0]*TMath::Gaus(x, [1], [2]) + [3]*TMath::Gaus(x, [4], [5])", 0, 170);
+    TF1 *gaussianSum = new TF1("gaussianSum", "[0]*TMath::Gaus(x, [1], [2]) + [3]*TMath::Gaus(x, [4], [5])", 0, 500);
 
-    gaussianSum->SetParameters(1.63229e+03, 1.15777e+02, 3.44590e+00, 2.38294e+02, 1.25212e+02, 1.88109e+00);
+    gaussianSum->SetParameters(1.42697e+04, 1.92995e+01, 2.09538e+00, 6.38699e+03, 4.16048e+02, 1.10978e+01);
 
     gaussianSum->SetLineColor(kSpring-8);
 
     gaussianSum->Draw("same");
+    */
 
     // Create a legend and add entries for the data, individual Gaussians, and the sum
     TLegend *legend = new TLegend(0.7,0.7,0.9,0.9);
     legend->AddEntry(graph, "Dados", "lep");
-    legend->AddEntry(gaussian1, "Pico de conversao", "l");
-    legend->AddEntry(gaussian2, "Pico", "l");
-    legend->AddEntry(gaussianSum, "Soma dos picos", "l");
+    legend->AddEntry(gaussian4, "Pico de retrodifusao 1", "l");
+    legend->AddEntry(gaussian3, "Pico de retrodifusao 2", "l");
+    legend->AddEntry(gaussian2, "Pico de absorcao total 1", "l");
+    legend->AddEntry(gaussian1, "Pico de absorcao total 2", "l");
+    
+
+    //legend->AddEntry(gaussianSum, "Soma dos picos", "l");
+
     legend->Draw();
 
     // Save the plot as a PDF file
